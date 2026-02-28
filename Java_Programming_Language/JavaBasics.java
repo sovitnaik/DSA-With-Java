d2be98d18ec960f0ef65cf3e89cf010239f98648
Sovit Naik <sovitnaik2007@gmail.com>
2026-03-01 00:49:15 +0530

    Initial commit - DSA with Java

---
+++ Java_Programming_Language/JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println("Hello World!");  //Add a Line Between this Line And Created a another line and Print There
+        System.out.println("Hello World!");
+   }
+}
+
+//This is Called as Boiler Plate Code Because This Can be Used Simultaneously in Every Java Code
