1cd83ceb835a80f54a73f30bbe8176ebcea04159
Sovit Naik <sovitnaik2007@gmail.com>
2026-03-10 00:05:09 +0530

    Added Variables and Datatypes Programs in Java

---
+++ Java_Programming_Language/DatatypesinJava.java
@@ -0,0 +1,18 @@
+public class DatatypesinJava {
+    public static void main(String args[]){
+        byte a = 10;
+        System.out.println(a);
+        char Name = 'a';
+        System.out.println(Name);
+        boolean var = true;
+        float val = 10.5f;
+        int number = 25;
+        //long :- This Can be Used for Long Number
+        //Double :- This can be Used for a Big Float Number
+        short n = 250;
+        System.out.println(var);
+        System.out.println(val);
+        System.out.println(number);
+        System.out.println(n);
+    }
+}
---
+++ Java_Programming_Language/VariablesinJava.java
@@ -0,0 +1,8 @@
+public class VariablesinJava {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        System.out.println("a");  // if we Take Double Quotes we it will Print a
+        System.out.println(a);  // Here it will Print the Value of a
+    }
+}
