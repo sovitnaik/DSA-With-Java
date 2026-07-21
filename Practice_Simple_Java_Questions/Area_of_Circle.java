e3a2237d3b188824438b960ed44574a37a0c816b
Sovit Naik <sovitnaik2007@gmail.com>
2026-07-21 23:30:55 +0530

    Area_of_Circle and Product_of_twonos

---
+++ Practice_Simple_Java_Questions/Area_of_Circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Area_of_Circle {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float r = sc.nextFloat();
+        float Area = 3.14f * r * r;
+        System.out.println(Area);
+    }
+}
